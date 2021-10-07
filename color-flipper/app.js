64b2391f237500ca4596fef079958bf968b591aa
MdTasib <ohidulalam442@gmail.com>
2021-10-07 22:04:43 +0600

    hex part start

--- color-flipper/app.js
+++ color-flipper/app.js
@@ -1,7 +1,26 @@
-const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+const colors = [
+	'green',
+	'red',
+	'rgba(133,122,200)',
+	'#f15025',
+	'cyan',
+	'yellow',
+	'gray',
+	'purple',
+	'pink',
+	'peru',
+];
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
 btn.addEventListener('click', function () {
-	console.log('color flipper');
+	// get random number between ( 0 - 3 )
+	const randomNumber = getRandomNumber();
+
+	document.body.style.background = colors[randomNumber];
+	color.textContent = colors[randomNumber];
 });
+
+function getRandomNumber() {
+	return Math.floor(Math.random() * colors.length);
+}
